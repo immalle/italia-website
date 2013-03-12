f833deb3da1d1c1376eea37226b3eba0acc89c83
Ewoud Van Looy <ewoud.vanlooy@immalle.be>
2013-03-12 11:43:45 +0100

    FotosUploaden

---
+++ Italia_1213/Italia_1213/FotosUploaden.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/ItaliaMasterPage.Master" CodeBehind="FotosUploaden.aspx.vb" Inherits="Italia_1213.FotosUploaden" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Calendar ID="calDatum" runat="server"></asp:Calendar>
+    <asp:FileUpload ID="fupUpload" runat="server" />
+    <asp:Button ID="btnUpload" runat="server" Text="Load Up" />
+    <asp:SqlDataSource ID="sqlDatasource" ConnectionString="<%$ ConnectionStrings:ItaliaConnection %>"
+        runat="server"></asp:SqlDataSource>
+</asp:Content>
+
---
+++ Italia_1213/Italia_1213/FotosUploaden.aspx.vb
@@ -0,0 +1,19 @@
+﻿Public Class FotosUploaden
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub btnUpload_Click(sender As Object, e As System.EventArgs) Handles btnUpload.Click
+        Dim strBestandsnaam As String = Date.Now.Year.ToString & "-" & Date.Now.Month.ToString & "-" & Date.Now.Day.ToString & "-" & Date.Now.Hour.ToString & "-" & Date.Now.Minute.ToString & "-" & Date.Now.Second.ToString & IO.Path.GetExtension(fupUpload.PostedFile.FileName)
+        Dim strPad As String = Server.MapPath("uploads\" & strBestandsnaam)
+        fupUpload.PostedFile.SaveAs(strPad)
+
+        sqlDatasource.InsertCommand = "INSERT INTO tblFotos (Naam,Dag) VALUES (@bestandsnaam,@datum)"
+        sqlDatasource.InsertParameters.Add("bestandsnaam", DbType.String, strBestandsnaam)
+        sqlDatasource.InsertParameters.Add("datum", DbType.Date, calDatum.SelectedDate.ToString)
+
+        sqlDatasource.Insert()
+    End Sub
+End Class
