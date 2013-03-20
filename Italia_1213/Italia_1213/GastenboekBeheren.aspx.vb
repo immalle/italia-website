6e60eb17f0050c36eb553ac3f1ff76edc3aeda8d
immalle <hans.vanbroeckhoven@immalle.be>
2013-03-20 14:26:25 +0100

    gastenboek crash

--- Italia_1213/Italia_1213/GastenboekBeheren.aspx.vb
+++ Italia_1213/Italia_1213/GastenboekBeheren.aspx.vb
@@ -3,9 +3,6 @@ Partial Class _Default
     Inherits System.Web.UI.Page
     
     Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
-        'If Session("id") = String.Empty Then
-        '    Response.Redirect("inloggen.aspx")
-        'End If
     End Sub
 End Class
 
