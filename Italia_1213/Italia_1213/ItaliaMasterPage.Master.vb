d5d76c3fd612ef5ae479dee4c595e398613b9d20
RobieKonijn <robin.janssens@immalle.be>
2013-03-20 09:27:31 +0100

    MenuItems aangepast d.m.v. code

--- Italia_1213/Italia_1213/ItaliaMasterPage.Master.vb
+++ Italia_1213/Italia_1213/ItaliaMasterPage.Master.vb
@@ -2,6 +2,23 @@
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        NavigationMenu.Items.Clear()
+
+        Dim homeItem = New MenuItem("Home", "", "", "~/Startpagina.aspx")
+        Dim dagverslagenItem = New MenuItem("Dagverslagen", "", "", "~/NieuwsBekijken.aspx")
+        Dim programmaItem = New MenuItem("Programma", "", "", "~/Programma.aspx")
+        Dim fotoItem = New MenuItem("Foto's", "", "", "~/Fotos.aspx")
+        Dim gastenboekItem = New MenuItem("Gastenboek", "", "", "~/Gastenboek.aspx")
+        Dim loginItem = New MenuItem("Inloggen", "", "", "~/Login.aspx")
+
+
+
+        NavigationMenu.Items.Add(homeItem)
+        NavigationMenu.Items.Add(dagverslagenItem)
+        NavigationMenu.Items.Add(programmaItem)
+        NavigationMenu.Items.Add(fotoItem)
+        NavigationMenu.Items.Add(gastenboekItem)
+        NavigationMenu.Items.Add(loginItem)
 
     End Sub
 End Class
