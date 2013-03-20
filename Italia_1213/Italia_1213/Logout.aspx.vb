77a176be2cda135ec745eaa67a8feaebe0077627
immalle <hans.vanbroeckhoven@immalle.be>
2013-03-20 13:44:58 +0100

    Startpagina hernoemd naar Default.aspx fix

--- Italia_1213/Italia_1213/Logout.aspx.vb
+++ Italia_1213/Italia_1213/Logout.aspx.vb
@@ -3,7 +3,7 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Session.Abandon()
-        Response.Redirect("~/Startpagina.aspx")
+        Response.Redirect("~/Default.aspx")
     End Sub
 
 End Class
