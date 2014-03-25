43237734d325dbf72c9e4713c11e6c605d433f2b
ToonDeDoncker <toon.dedoncker@immalle.be>
2014-03-25 11:17:00 +0100

    ID van plaats
    De ID van de plaats van het programma van die dag bij in 'marker'
    object.
    Nu kan er database aangemaakt worden met id's voor de plaatsen. Enz...

--- Italia_1314/ItalieNieuw/Scripts/googleMap.js
+++ Italia_1314/ItalieNieuw/Scripts/googleMap.js
@@ -1,14 +1,23 @@
-﻿
+﻿$(document).ready(function () {
+    var getDagInfo = function (marker) {
+
+
+
+
+        alert(marker.id);
+
+    };
+
 var locations = [
-     ['Immaculata', 51.302518, 4.734758, 1],
-     ['Pisa', 43.722731, 10.405369, 2],
-     ['Firenze', 43.773108, 11.255769, 3],
-     ['Assisi', 43.074421, 12.606314, 2],
-     ['Rome', 41.902085, 12.457452, 2],
-     ['Sienna', 43.318512, 11.331493],
-     ['Chianciano', 43.043865, 11.812091],
-     ['Lido Di Jesolo', 45.505655, 12.645042],
-     ['Venetie', 45.439297, 12.326288]];
+     [0, 'Immaculata', 51.302518, 4.734758, 1],
+     [1, 'Pisa', 43.722731, 10.405369, 2],
+     [2, 'Firenze', 43.773108, 11.255769, 3],
+     [3, 'Assisi', 43.074421, 12.606314, 2],
+     [4, 'Rome', 41.902085, 12.457452, 2],
+     [5, 'Sienna', 43.318512, 11.331493],
+     [6, 'Chianciano', 43.043865, 11.812091],
+     [7, 'Lido Di Jesolo', 45.505655, 12.645042],
+     [8, 'Venetie', 45.439297, 12.326288]];
 // Markers + Textballon met naam
 var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
@@ -21,18 +30,23 @@ var infowindow = new google.maps.InfoWindow();
 var marker, i;
 for (i = 0; i < locations.length; i++) {
     marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        position: new google.maps.LatLng(locations[i][2], locations[i][3]),
         map: map,
         animation: google.maps.Animation.DROP,
-
+        id: locations[i][0]
     });
 
     google.maps.event.addListener(marker, 'click', (function (marker, i) {
+        
         return function () {
-            infowindow.setContent(locations[i][0]);
+            infowindow.setContent(locations[i][1]);
             infowindow.open(map, marker);
+            getDagInfo(marker);
+            
         }
     })(marker, i));
 
 }
 
+
+});
