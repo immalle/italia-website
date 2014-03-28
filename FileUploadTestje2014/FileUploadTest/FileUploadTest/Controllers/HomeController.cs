9749d88663d8b80c6972c457e43e399fdd11b41f
immalle <hans.vanbroeckhoven@immalle.be>
2014-03-28 16:15:00 +0100

    Single file upload + foto viewer werkend, Multiple file upload nog niet

--- FileUploadTestje2014/FileUploadTest/FileUploadTest/Controllers/HomeController.cs
+++ FileUploadTestje2014/FileUploadTest/FileUploadTest/Controllers/HomeController.cs
@@ -10,47 +10,63 @@ namespace FileUploadTest.Controllers
 {
     public class HomeController : Controller
     {
-        // This action renders the form
         public ActionResult Index()
         {
+            // Toon alle huidige foto's
+
+            //Regex reg = new Regex(@"image(\d)+[.]");
+
+            //if (list.Count == 0)
+            //    return "image1.jpg";
+
+            //var lastName =
+            //    list.Select(x => (new FileInfo(x)).Name.Replace("image", "").Replace(".jpg", "")).OrderBy(x => x).Last();
+
+            //string.Format("image{0}.png", int.Parse(lastName) + 1);
+
+
+            //List<string> jpgFiles = new List<string>();
+
+            //jpgFiles.Add("~/ImageUploads/image1.jpg");
+            //jpgFiles.Add("~/ImageUploads/image2.jpg");
+
+            //ViewBag.jpgFiles = jpgFiles;
+
+            List<string> jpgFiles;
+            jpgFiles = Directory.GetFiles(Server.MapPath("/ImageUploads/"), "*.jpg").ToList<string>();
+            
+            ViewBag.jpgFiles = jpgFiles;
             return View();
         }
 
         // This action handles the form POST and the upload
         [HttpPost]
-        public ActionResult Index(HttpPostedFileBase file)
+        public ActionResult UploadFile(HttpPostedFileBase file)
         {
-            // Verify that the user selected a file
             if (file != null && file.ContentLength > 0)
             {
-                // extract only the filename
                 var fileName = Path.GetFileName(file.FileName);
-                // store the file inside the right folder
                 var path = Path.Combine(Server.MapPath("~/ImageUploads/"), fileName);
                 file.SaveAs(path);
-
-                //Regex reg = new Regex(@"image(\d)+[.]");
-
-                //var list = Directory.GetFiles(fileName, "*.jpg").Where(path => reg.IsMatch(path))
-                //         .ToList();
-
-                //if (list.Count == 0)
-                //    return "image1.jpg";
-
-                //var lastName =
-                //    list.Select(x => (new FileInfo(x)).Name.Replace("image", "").Replace(".jpg", "")).OrderBy(x => x).Last();
-
-                //string.Format("image{0}.png", int.Parse(lastName) + 1);
-
-                
-                
-                
-                
-            }
-            // redirect back to the index action to show the form once again
+            }                
+        
             return RedirectToAction("Index");
         }
 
+        [HttpPost]
+        public ActionResult UploadMultipleFiles(HttpPostedFileBase[] files)
+        {
+            foreach(var file in files)
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/ImageUploads/"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+            return RedirectToAction("Index");
+        }
 
         public ViewResult About()
         {
