0e97c827d51a4ada8235bb422e1abe8b71fabd99
vbrh_immalle <hans.vanbroeckhoven@immalle.be>
2014-03-31 18:58:05 +0200

    Dagverslagen werkend

---
+++ Italia_1314/ItalieNieuw/Controllers/DailyReportController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ItalieNieuw.Models;
+
+namespace ItalieNieuw.Controllers
+{
+    [Authorize]
+    public class DailyReportController : Controller
+    {
+        private ItaliaDb db = new ItaliaDb();
+
+        //
+        // GET: /DailyReport/
+
+        public ActionResult Index()
+        {
+            return View(db.DailyReports.ToList());
+        }
+
+        //
+        // GET: /DailyReport/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            DailyReport dailyreport = db.DailyReports.Find(id);
+            if (dailyreport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dailyreport);
+        }
+
+        //
+        // GET: /DailyReport/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /DailyReport/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DailyReport dailyreport)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DailyReports.Add(dailyreport);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dailyreport);
+        }
+
+        //
+        // GET: /DailyReport/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            DailyReport dailyreport = db.DailyReports.Find(id);
+            if (dailyreport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dailyreport);
+        }
+
+        //
+        // POST: /DailyReport/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(DailyReport dailyreport)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dailyreport).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dailyreport);
+        }
+
+        //
+        // GET: /DailyReport/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            DailyReport dailyreport = db.DailyReports.Find(id);
+            if (dailyreport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dailyreport);
+        }
+
+        //
+        // POST: /DailyReport/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DailyReport dailyreport = db.DailyReports.Find(id);
+            db.DailyReports.Remove(dailyreport);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- Italia_1314/ItalieNieuw/Models/DailyReport.cs
+++ Italia_1314/ItalieNieuw/Models/DailyReport.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ItalieNieuw.Models
 {
     public class DailyReport
     {
         public int Id { get; set; }
-        // TODO : Datum anders weergeven. SOLVED
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+        [DataType(DataType.MultilineText)]
+        [AllowHtml]
         public String Report { get; set; }
         public String Title { get; set; }
-
     }
 }
